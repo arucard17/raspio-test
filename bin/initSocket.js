6899fdd2f05aec4a4fce0a4aa59778db78fa54b3
arucard17 <kamui_shirou99@hotmail.com>
2014-12-06 11:53:59 -0500

    Prueba Speech Recognition

--- bin/initSocket.js
+++ bin/initSocket.js
@@ -7,6 +7,14 @@ module.exports = function (server, board){
             // Set pin 7's output to logic high
             board.pins[7].value = data.st ? board.HIGH : board.LOW;
         });
+
+        socket.on('speechRec', function(data) {
+            if(data.speech == 'encender')
+                board.pins[7].value = board.HIGH;
+            else
+                board.pins[7].value = board.LOW;
+        });
+
     });
 
 };
--- public/js/voice.js
+++ public/js/voice.js
@@ -1,24 +1,93 @@
 $(function() {
     var $btn = $('#btnListen'),
         $listen = $('#listen h3'),
-        status = false;
+        $result = $('#result'),
+        status = false,
+        socket = io.connect('http://192.168.0.103');
 
-    $btn.on('click', function() {
-        status = !status;
-        changeStatus();
-    });
+    window.SpeechRecognition = window.SpeechRecognition ||
+        window.webkitSpeechRecognition ||
+        null;
+    if (window.SpeechRecognition == null) {
+        alert('Este explorador no soporta Speech Recognition');
+    } else {
+        init();
+    }
 
-    function changeStatus() {
-        if (status) {
-            $btn.removeClass().addClass('animated flash active');
-            $listen.addClass('active');
-        } else {
-            $btn
-                .removeClass('animated flash')
-                .addClass('animated shake')
-                .removeClass('active');
+    function init() {
+        var recognizer = new window.SpeechRecognition();
 
-            $listen.removeClass('active');
+        recognizer.continuous = true;
+        recognizer.lang = "es-CO";
+
+        // Start recognising
+        recognizer.onresult = function(event) {
+            $result.html('');
+            var str = '';
+            for (var i = event.resultIndex; i < event.results.length; i++) {
+                if (event.results[i].isFinal) {
+                    str = event.results[i]
+                        [0].transcript + ' (Confidence: ' +
+                        event.results[i][0].confidence + ')';
+                    emitServer(str);
+                } else {
+                    str += event.results[
+                        i][0].transcript;
+                }
+                console.log(str);
+            }
+        };
+
+        // Listen for errors
+        recognizer.onerror = function(event) {
+            console.log('Recognition error: ' + event.message);
+        };
+
+        function start() {
+            // Set if we need interim results
+            recognizer.interimResults = 'interim';
+            try {
+                recognizer.start();
+            } catch (ex) {
+                console.log('Recognition error: ' + ex.message);
+            }
+        }
+
+        function stop() {
+            recognizer.stop();
+            console.log('Recognition stopped');
+        }
+
+
+        $btn.on('click', function() {
+            status = !status;
+            changeStatus();
+        });
+
+        function changeStatus() {
+            if (status) {
+                start();
+
+                $btn.removeClass().addClass(
+                    'animated flash active');
+                $listen.addClass('active');
+            } else {
+                stop();
+
+                $btn
+                    .removeClass('animated flash')
+                    .addClass('animated shake')
+                    .removeClass('active');
+
+                $listen.removeClass('active');
+            }
         }
     }
+    
+    function emitServer(str){
+        socket.emit('speechRec', {
+            speech: str
+        });
+    }
+
 });
