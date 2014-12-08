f897d802b1e4a846bec931214117e96d947f1d01
arucard17 <kamui_shirou99@hotmail.com>
2014-12-07 22:09:12 -0500

    Agregado comentarios. Optimización para enviar el resultado obtenido al servidor.

--- public/js/voice.js
+++ public/js/voice.js
@@ -8,6 +8,8 @@ $(function() {
     window.SpeechRecognition = window.SpeechRecognition ||
         window.webkitSpeechRecognition ||
         null;
+        
+    // Validar que el explorador tenga el soporte para el API
     if (window.SpeechRecognition == null) {
         alert('Este explorador no soporta Speech Recognition');
     } else {
@@ -20,23 +22,14 @@ $(function() {
         recognizer.continuous = true;
         recognizer.lang = "es-CO";
 
-        // Start recognising
-        recognizer.onresult = function(event) {
-            var str = '';
-            for (var i = event.resultIndex; i < event.results.length; i++) {
-                if(str != event.results[i][0].transcript){
-                    str = event.results[i][0].transcript;
-                    console.log(cleanResult(str));
-                    emitServer(cleanResult(str));
-                }
-            }
-        };
+        // Evento para cambiar el estado entre "Reconociendo Voz" y "Apagado"
+        $btn.on('click', function() {
+            status = !status;
+            changeStatus();
+        });
 
-        // Listen for errors
-        recognizer.onerror = function(event) {
-            console.log('Recognition error: ' + event.message);
-        };
 
+        // Iniciar Reconocimiento
         function start() {
             // Set if we need interim results
             recognizer.interimResults = 'interim';
@@ -47,21 +40,31 @@ $(function() {
             }
         }
 
+        // Para reconocimiento
         function stop() {
             recognizer.stop();
             console.log('Recognition stopped');
         }
 
+        // Método que será ejecutado cuando haya información de lectura de voz
+        recognizer.onresult = function(event) {
+            for (var i = event.resultIndex; i < event.results.length; i++) {
+                // console.log(cleanResult(event.results[i][0].transcript));
+                emitServer(clsRs(event.results[i][0].transcript));
+            }
+        };
 
-        $btn.on('click', function() {
-            status = !status;
-            changeStatus();
-        });
+        // Mostrar Error
+        recognizer.onerror = function(event) {
+            console.log('Recognition error: ' + event.message);
+        };
 
+        // Función para cambiar el estado
         function changeStatus() {
             if (status) {
                 start();
 
+                // Aplicando animación para mostrar que se empezó el proceso de reconocimiento de voz
                 $btn
                     .removeClass()
                     .addClass('animated flash active');
@@ -70,6 +73,7 @@ $(function() {
             } else {
                 stop();
 
+                // Aplicando animación para indicar que se ha parado el proceso
                 $btn
                     .removeClass('animated flash')
                     .addClass('animated shake')
@@ -80,13 +84,15 @@ $(function() {
         }
     }
 
+    // Función que envía la información obtenida por el reconocimiento de voz al servidor
     function emitServer(str) {
         socket.emit('speechRec', {
             speech: str
         });
     }
 
-    function cleanResult(str){
+    // Función para limpiar el espacio en blanco de los resultados obtenidos por el reconocedor de voz
+    function clsRs(str){
         return str.replace(' ', '');
     }
 
