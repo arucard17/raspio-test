7635a4f29b153a4784d4032497a64ceca242dacb
arucard17 <kamui_shirou99@hotmail.com>
2014-12-07 21:57:43 -0500

    Arreglo de validación, no es igual, es diferente, para así aceptar los comandos diferentes al anterior.

--- public/js/voice.js
+++ public/js/voice.js
@@ -24,7 +24,7 @@ $(function() {
         recognizer.onresult = function(event) {
             var str = '';
             for (var i = event.resultIndex; i < event.results.length; i++) {
-                if(str == event.results[i][0].transcript){
+                if(str != event.results[i][0].transcript){
                     str = event.results[i][0].transcript;
                     console.log(cleanResult(str));
                     emitServer(cleanResult(str));
